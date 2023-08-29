833f995b98507d6cba9ef4870256931f78d2a4f6
Dayaldonadkar <Dayalpython.com>
2023-08-29 16:59:31 +0530

    Basko 2nd navbar added

--- src/components/Navbar.js/Navbar5.js
+++ src/components/Navbar.js/Navbar5.js
@@ -18,19 +18,19 @@ const Navbar5 = ({
     <main>
       <div className="flex justify-between items-center px-5 lg:px-20 py-4 relative ">
         <div className="flex item-center justify-between w-[100%]">
-          <div className="space-x-10 -z-10">
+          <div className="flex items-center space-x-10 -z-10">
             <img src={img} alt="" />
+            <ul className="flex font-light items-center text-sm space-x-3">
+              <l className="px-3">{li1}</l>
+              <li className="px-3">{li2}</li>
+              <li className="">{li3}</li>
+            </ul>
           </div>
           <div className="hidden md:block pt-2">
             <div className="flex items-center space-x">
-              <ul className="flex font-light items-center text-sm space-x-3">
-                <l className="px-3">{li1}</l>
-                <li className="px-3">{li2}</li>
-                <li className="border-r border-r-black pr-3">{li3}</li>
-              </ul>
-              <div>
-                <h6>{li4}</h6>
-                <button className="text-md md:text-xs px-2 rounded-lg bg-indigo-500 text-white hover:bg-indigo-600 py-2">
+              <div className="flex items-center text-light text-sm space-x-5">
+                <h6 className="text-sm font-light">{li4}</h6>
+                <button className="text-md md:text-xs  rounded-lg bg-indigo-500 text-white hover:bg-indigo-600 py-2 px-2">
                   {button1}
                 </button>
               </div>
